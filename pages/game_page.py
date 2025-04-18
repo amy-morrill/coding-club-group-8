1e334833202a0f707ac8de9ce69dfed92c4bd3cc
michellehuangtempus <123395426+michellehuangtempus@users.noreply.github.com>
2025-04-18 13:13:43 -0400

    Add to game page

--- pages/game_page.py
+++ pages/game_page.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import random
 
-st.title("GAME PAGE")
+st.title("Build a Scientist")
+
+# Display scientist  
+# st.image("images/scientist-beaker-test-tube-animated-f.gif")
+# Display scientist with equipment 
+
+game_words = ['Genetic','Histology','Sequence','Clinical Trial','Mutation','Genome','Oncology']
+
+def game_word_generator(word):
+    st.write("".join("_" * len(word)))
+
+if game_words:
+    selected_word = random.choice(game_words)
+    game_word_generator(selected_word)
