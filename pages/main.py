d9ab28c7f19947a6fba09ec5b360ad8110541184
Amy Morrill <amy.morrill@tempus.com>
2025-04-25 14:26:05 -0400

    fix main

--- pages/main.py
+++ pages/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 page_1 = st.Page("opening_page.py", title="Opening page")
 page_2 = st.Page("game_page.py", title="Game page")
-page_3 = st.Page("end_page.py", title="End page")
+page_3 = st.Page("win_page.py", title="Win page")
+page_4 = st.Page("lose_page.py", title = "Lost page")
 pages = st.navigation([page_1, page_2], position="hidden")
 pages.run()
