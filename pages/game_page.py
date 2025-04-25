b4887aa2fa369d65d6df8144f8149a68d0fb6442
michellehuangtempus <123395426+michellehuangtempus@users.noreply.github.com>
2025-04-25 13:42:35 -0400

    Add to game page (#12)
    * Add to game page
    
    * Fix game_word_generator part
    
    * Add additional game page elements
    
    * Additional updates to fix session state
    
    * Fix session state and add eval_guess
    
    * Good guess vs bad guess
    
    * add lower case
    
    * Simplify the game_word_generator function
    
    * Corrections to lower case guesses and limit to single character guesses

--- pages/game_page.py
+++ pages/game_page.py
@@ -1,3 +1,47 @@
 import streamlit as st
+import random 
 
-st.title("GAME PAGE")
+st.title("Build a Scientist")
+
+game_words = ['Genetic','Histology','Sequence','Mutation','Genome','Oncology']
+
+
+if game_words:
+    selected_word = random.choice(game_words)
+
+# Session state
+if 'selected_word' not in st.session_state:
+    st.session_state.selected_word = selected_word
+    
+if 'good_guesses' not in st.session_state:
+    st.session_state.good_guesses = []
+
+if 'bad_guesses' not in st.session_state:
+    st.session_state.bad_guesses = []
+
+# Display and update the game word as player enters new guesses 
+def game_word_generator(word):
+    st.write(word)
+    # Create a display string with underscores for each letter in the word
+    final_word = ' '.join([guess if guess.lower() in st.session_state.good_guesses else '_' for guess in word])
+    st.write(final_word)
+    
+# st.write(st.session_state.good_guesses)
+# st.write(st.session_state.bad_guesses)
+
+game_word_generator(st.session_state.selected_word)
+
+# Create function to evaluate the guess
+def eval_guess():
+    guess = st.session_state.guess
+    if len(guess) == 1:
+        if guess in st.session_state.selected_word:
+            st.session_state.good_guesses.append(guess) 
+            st.success("Good guess!")
+        else: 
+            st.session_state.bad_guesses.append(guess)
+            st.error("Try again") 
+    else :
+        st.error("Guess must be one character long.")
+
+guess = st.text_input("Guess a letter:", key="guess", on_change=eval_guess)
