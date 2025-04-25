0e2248c7ada793e5271e82350928794c3298e0d7
Amy Morrill <amy.morrill@tempus.com>
2025-04-25 14:11:33 -0400

    add inventory

--- pages/main.py
+++ pages/main.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 page_1 = st.Page("opening_page.py", title="Opening page")
 page_2 = st.Page("game_page.py", title="Game page")
+page_3 = st.Page("end_page.py", title="End page")
 pages = st.navigation([page_1, page_2], position="hidden")
 pages.run()
