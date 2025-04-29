c447397ffccc82545fc1987c9363a91dc50b2526
Amy Morrill <amy.morrill@tempus.com>
2025-04-29 17:35:40 -0400

    add emoji

--- pages/lose_page.py
+++ pages/lose_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_extras.let_it_rain import rain
-rain(emoji="",font_size=54, falling_speed=5)
+rain(emoji="😢",font_size=54, falling_speed=5)
 st.markdown("<h1 style=’text-align: center; color: red;’>Oh No!!!</h1>", unsafe_allow_html=True)
 st.write("I’m Sorry! We have no more inventory to complete this mission. Please try again to save the scientist!")
 if st.button("Solve for more words!"):
